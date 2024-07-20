2702fde1895251ca0e2b223ba5f0324e48c8eaf0
Alhassan Bawie Abu <abubawie.24@gmil.com>
2024-07-20 22:55:22 +0000

    removing div content

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ for (let i = 0; i < 257; i++){
 
     const div = document.createElement("div");
     div.classList.add("box");
-    div.textContent = `div${i + 0}`;
+    
     container.appendChild(div);
    
 }
