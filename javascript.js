ff1239089d187ad8c84bd94d7ceb3f901262cb43
Alhassan Bawie Abu <abubawie.24@gmil.com>
2024-07-22 01:03:19 +0000

    setting up hover effect fordivs

--- javascript.js
+++ javascript.js
@@ -1,11 +1,27 @@
+document.addEventListener(`DOMContentLoaded`, () => {
+
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 257; i++){
+for (let i = 0; i < 256; i++){
 
     const div = document.createElement("div");
     div.classList.add("box");
-    
+    div.classList.add("grid-item");
+
     container.appendChild(div);
-   
 }
+
+container.addEventListener(`mouseover`, (e) => {
+    if(e.target.classList.contains(`grid-item`)){
+        e.target.style.background = `black`;
+    }else{
+        e.target.style.background = `white`;
+    }
+})
+
+})
+
+
+
+
 
